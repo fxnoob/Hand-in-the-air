acc07ba29ec989d20fb508cf9b470d646b9463f6
fxnoob <fxnoob71@gmail.com>
2019-08-29 23:19:35 +0530

    icons and help page

--- option-page/components/index.jsx
+++ option-page/components/index.jsx
@@ -8,6 +8,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Gest from "../../src/lib/gest.es6";
 import Img from './help.png'
+import Img2 from './help2.png'
 import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles({
@@ -17,12 +18,14 @@ const useStyles = makeStyles({
     media: {
         height: 140,
     },
+    media2: {
+        height: 470,
+    }
 });
 
 const gest = new Gest();
 gest.options.subscribeWithCallback((gesture) => {
     console.log(gesture);
-
 })
 gest.start()
 
@@ -30,16 +33,18 @@ export default function MediaCard() {
     const classes = useStyles();
 
     return (
+        <div>
         <Container maxWidth="sm" spacing={3}>
+            <h1>Help</h1>
             <Card className={classes.card}>
                 <CardActionArea>
                     <CardMedia
                         className={classes.media}
                         image={Img}
-                        title="Contemplative Reptile"
+                        title="Troubleshooting"
                     />
                     <CardContent>
-                        Allow Camera permission as shown in the picture.
+                        Allow Camera permission after that close this tab and click on extension's icon again.
                     </CardContent>
                 </CardActionArea>
                 <CardActions>
@@ -52,5 +57,31 @@ export default function MediaCard() {
                 </CardActions>
             </Card>
         </Container>
+        <Container maxWidth="sm" spacing={3}>
+            <h1>Features</h1>
+            <Card className={classes.card}>
+                <CardActionArea>
+                    <CardMedia
+                        className={classes.media2}
+                        image={Img2}
+                        title="Features"
+                    />
+                    <CardContent>
+                        <p>Wave hand left to right to switch active tab towards right.</p>
+                        <p>Wave hand right to left to switch active tab towards left.</p>
+                        <p>Wave hand bottom to top to close active tab.</p>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Share
+                    </Button>
+                    <Button size="small" color="primary">
+                        Learn More
+                    </Button>
+                </CardActions>
+            </Card>
+        </Container>
+        </div>
     );
 }
