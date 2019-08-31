952364fae79389f7da95df0bfd2e8814558e3837
fxnoob <fxnoob71@gmail.com>
2019-08-31 18:35:58 +0530

    format code with prettier

--- option-page/components/index.jsx
+++ option-page/components/index.jsx
@@ -1,87 +1,88 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
 import Gest from "../../src/lib/gest.es6";
-import Img from './help.png'
-import Img2 from './help2.png'
-import Container from '@material-ui/core/Container';
+import Img from "./help.png";
+import Img2 from "./help2.png";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles({
-    card: {
-        maxWidth: 800,
-    },
-    media: {
-        height: 140,
-    },
-    media2: {
-        height: 470,
-    }
+  card: {
+    maxWidth: 800
+  },
+  media: {
+    height: 140
+  },
+  media2: {
+    height: 470
+  }
 });
 
 const gest = new Gest();
-gest.options.subscribeWithCallback((gesture) => {
-    console.log(gesture);
-})
-gest.start()
+gest.options.subscribeWithCallback(gesture => {
+  console.log(gesture);
+});
+gest.start();
 
 export default function MediaCard() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div>
-        <Container maxWidth="sm" spacing={3}>
-            <h1>Help</h1>
-            <Card className={classes.card}>
-                <CardActionArea>
-                    <CardMedia
-                        className={classes.media}
-                        image={Img}
-                        title="Troubleshooting"
-                    />
-                    <CardContent>
-                        Allow Camera permission after that close this tab and click on extension's icon again.
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                        Share
-                    </Button>
-                    <Button size="small" color="primary">
-                        Learn More
-                    </Button>
-                </CardActions>
-            </Card>
-        </Container>
-        <Container maxWidth="sm" spacing={3}>
-            <h1>Features</h1>
-            <Card className={classes.card}>
-                <CardActionArea>
-                    <CardMedia
-                        className={classes.media2}
-                        image={Img2}
-                        title="Features"
-                    />
-                    <CardContent>
-                        <p>Wave hand left to right to switch active tab towards right.</p>
-                        <p>Wave hand right to left to switch active tab towards left.</p>
-                        <p>Wave hand bottom to top to close active tab.</p>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                        Share
-                    </Button>
-                    <Button size="small" color="primary">
-                        Learn More
-                    </Button>
-                </CardActions>
-            </Card>
-        </Container>
-        </div>
-    );
+  return (
+    <div>
+      <Container maxWidth="sm" spacing={3}>
+        <h1>Help</h1>
+        <Card className={classes.card}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image={Img}
+              title="Troubleshooting"
+            />
+            <CardContent>
+              Allow Camera permission after that close this tab and click on
+              extension's icon again.
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
+      </Container>
+      <Container maxWidth="sm" spacing={3}>
+        <h1>Features</h1>
+        <Card className={classes.card}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media2}
+              image={Img2}
+              title="Features"
+            />
+            <CardContent>
+              <p>Wave hand left to right to switch active tab towards right.</p>
+              <p>Wave hand right to left to switch active tab towards left.</p>
+              <p>Wave hand bottom to top to close active tab.</p>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
+      </Container>
+    </div>
+  );
 }
--- src/background.js
+++ src/background.js
@@ -1,79 +1,81 @@
-import "@babel/polyfill"
-import Gest from './lib/gest.es6'
-import ChromeApi from './lib/chromeApi'
-import Db, { Schema } from "./lib/db"
+import "@babel/polyfill";
+import Gest from "./lib/gest.es6";
+import ChromeApi from "./lib/chromeApi";
+import Db, { Schema } from "./lib/db";
 
-let AppInitState = 0 // it means app is off on startup
+let AppInitState = 0; // it means app is off on startup
 
 const gest = new Gest();
-const chromeObj = new ChromeApi()
-const schema = new Schema()
-const db = new Db()
+const chromeObj = new ChromeApi();
+const schema = new Schema();
+const db = new Db();
 
-class Main extends ChromeApi{
-    constructor() {
-        super()
+class Main extends ChromeApi {
+  constructor() {
+    super();
+  }
+
+  init = async () => {
+    await this.initDb();
+    this.setUpTabSwipe();
+    this.popUpClickSetup();
+  };
+
+  initDb = async () => {
+    const res = await db.get(["loadedFirstTine"]);
+    if (!res.hasOwnProperty("loadedFirstTime")) {
+      await db.set({ loadedFirstTime: true, ...schema.data });
     }
+  };
 
-    init = async () => {
-        await this.initDb()
-        this.setUpTabSwipe()
-        this.popUpClickSetup()
-    }
-
-    initDb = async () => {
-        const res = await db.get(["loadedFirstTine"]);
-        if(!res.hasOwnProperty("loadedFirstTime")) {
-            await db.set({loadedFirstTime: true, ...schema.data})
+  setUpTabSwipe = () => {
+    gest.options.subscribeWithCallback(gesture => {
+      console.log({ gesture });
+      if (gesture.error) {
+        AppInitState = 0;
+        chromeObj.openHelpPage();
+      } else {
+        if (gesture.direction === "Left") {
+          console.log("Left");
+          chromeObj.shiftToLeftTab();
+        } else if (gesture.direction === "Right") {
+          console.log("Right");
+          chromeObj.shiftToRightTab();
+        } else if (gesture.direction === "Long up") {
+          console.log("Long up");
+          chromeObj.closeActiveTab();
         }
-    }
+      }
+    });
+  };
 
-    setUpTabSwipe = () => {
-        gest.options.subscribeWithCallback((gesture) => {
-            console.log({gesture})
-            if(gesture.error) {
-                AppInitState = 0
-               chromeObj.openHelpPage()
-            } else if(gesture.direction === "Left") {
-                console.log("Left")
-                chromeObj.shiftToLeftTab()
-            } else if (gesture.direction === "Right") {
-                console.log("Right")
-                chromeObj.shiftToRightTab()
-            } else if (gesture.direction === "Long up") {
-                console.log("Long up")
-                chromeObj.closeActiveTab()
-            }
-        })
-    }
+  popUpClickSetup() {
+    chrome.browserAction.onClicked.addListener(tab => {
+      if (this.toggleApp()) {
+        gest.start();
+      } else {
+        this.stopApp();
+      }
+    });
+  }
 
-    popUpClickSetup() {
-        chrome
-            .browserAction
-            .onClicked
-            .addListener((tab) => {
-                if(this.toggleApp()) {
-                    gest.start();
-                } else {
-                    this.stopApp()
-                }
-            });
-    }
+  toggleApp = () => {
+    AppInitState = AppInitState ? 0 : 1;
+    return AppInitState;
+  };
 
-    toggleApp = () => {
-      AppInitState = AppInitState?0:1;
-      return AppInitState
-    }
-
-    stopApp = () => {
-       AppInitState = 0
-        gest.stop();
-    }
+  stopApp = () => {
+    AppInitState = 0;
+    gest.stop();
+  };
 }
 
-
-const main = new Main()
+const main = new Main();
 main
-    .init()
-    .then(res => {console.log({res})})
-    .catch(e=> {console.log({e})});
+  .init()
+  .then(res => {
+    console.log({ res });
+  })
+  .catch(e => {
+    console.log({ e });
+  });
