3dff7f90cd7d840494109ea4e741bfe1cf2b4d0d
fxnoob <fxnoob71@gmail.com>
2019-09-06 03:26:03 +0530

    acknowledgement

--- src/lib/gest.es6.js
+++ src/lib/gest.es6.js
@@ -1,3 +1,30 @@
+/*
+ * @name: gest.es6.js
+ * @description: gest.es6.js is a webcam based gesture recognition library that helps developers make webpages more immersive
+ * @version: 0.5.0
+ * @author: Hitesh Saini (https://github.com/fxnoob)
+ * @acknowledgements: gest.js is an extension of work M. Hadi(http://hadi.io) which is extension of work started by William Wu (https://github.com/wvvvw)
+ * @license: MIT License
+	The MIT License (MIT)
+	Copyright (c) 2013-2014 Hadi Michael (http://hadi.io)
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
+
+
 //setup getUserMedia - this could screw up another implementation of getUserMedia on the page, but really, they shouldn't be using the camera for anything else
 window.navigator.getUserMedia =
   navigator.getUserMedia ||
