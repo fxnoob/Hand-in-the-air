d3127c5befd94b6f722e424b640404bc58c048c5
fxnoob <fxnoob71@gmail.com>
2019-09-06 03:04:24 +0530

    scroll plugin for google. play game 2048 with hand gestures on https://play2048.co/

---
+++ plugins/google/plugin_fxnoob.js
@@ -0,0 +1,16 @@
+if (!gesture.error) {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTo;
+  if (gesture.direction === "Long up") {
+    window.scrollTo({
+      'behavior': 'smooth',
+      'left': 0,
+      'top': scrollTop-350
+    });
+  } else if (gesture.direction === "Long down") {
+    window.scrollTo({
+      'behavior': 'smooth',
+      'left': 0,
+      'top': scrollTop+350
+    });
+  }
+}
