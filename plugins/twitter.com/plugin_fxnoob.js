d879c5938f16d634b8d814e901bbcba5c037a3d4
fxnoob <fxnoob71@gmail.com>
2019-11-03 21:57:13 +0530

    add prettier

--- plugins/twitter.com/plugin_fxnoob.js
+++ plugins/twitter.com/plugin_fxnoob.js
@@ -2,15 +2,15 @@ if (!gesture.error) {
   var scrollTop = window.pageYOffset || document.documentElement.scrollTo;
   if (gesture.direction === "Long up") {
     window.scrollTo({
-      'behavior': 'smooth',
-      'left': 0,
-      'top': scrollTop+350
+      behavior: "smooth",
+      left: 0,
+      top: scrollTop + 350
     });
   } else if (gesture.direction === "Long down") {
     window.scrollTo({
-      'behavior': 'smooth',
-      'left': 0,
-      'top': scrollTop-350
+      behavior: "smooth",
+      left: 0,
+      top: scrollTop - 350
     });
   }
 }
