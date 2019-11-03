ffe2e2390d41c48e7b4bf858e4b1d34391181252
fxnoob <fxnoob71@gmail.com>
2019-11-03 21:44:00 +0530

    sound mute unmute default plugin

---
+++ src/default_plugins/mute_unmute.js
@@ -0,0 +1,41 @@
+
+const name = "Sound Mute/Unmute";
+
+class MuteUnmute {
+  constructor() {
+    this.muted = [];
+  }
+  muteElement = (elem) => {
+    if(!elem.muted) {
+      elem.muted = true;
+      elem.pause();
+      this.muted.push(elem);
+    }
+  }
+  mutePage = () => {
+    const elems = document.querySelectorAll("video, audio");
+    [].forEach.call(elems, (elem) => { this.muteElement(elem); });
+  }
+  unmutePage = () => {
+    this.muted.map(elem => {
+      elem.muted = false;
+      elem.play();
+    });
+    this.muted = [];
+  }
+}
+
+const mu = new MuteUnmute();
+
+const exec = gesture => {
+  if (gesture.direction === "Left") {
+      mu.mutePage();
+  } else if (gesture.direction === "Right") {
+    mu.unmutePage();
+  }
+}
+
+export default {
+  name,
+  exec
+};
